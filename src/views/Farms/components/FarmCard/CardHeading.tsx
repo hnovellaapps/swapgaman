fbfc7b57162c4beb82e9648e4ae0541ea1dcba0a
Jan Der Klander <hnovella.apps@gmail.com>
2021-07-20 23:27:39 +0200

    swap pcke

---
+++ src/views/Farms/components/FarmCard/CardHeading.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Tag, Flex, Heading, TokenPairImage } from '@pancakeswap/uikit'
+import { CommunityTag, CoreTag } from 'components/Tags'
+import { Token } from 'config/constants/types'
+import { getAddress } from 'utils/addressHelpers'
+
+export interface ExpandableSectionProps {
+  lpLabel?: string
+  multiplier?: string
+  isCommunityFarm?: boolean
+  token: Token
+  quoteToken: Token
+}
+
+const Wrapper = styled(Flex)`
+  svg {
+    margin-right: 4px;
+  }
+`
+
+const MultiplierTag = styled(Tag)`
+  margin-left: 4px;
+`
+
+const CardHeading: React.FC<ExpandableSectionProps> = ({ lpLabel, multiplier, isCommunityFarm, token, quoteToken }) => {
+  return (
+    <Wrapper justifyContent="space-between" alignItems="center" mb="12px">
+      <TokenPairImage
+        variant="inverted"
+        primaryTokenAddress={getAddress(token.address)}
+        secondaryTokenAddress={getAddress(quoteToken.address)}
+        width={64}
+        height={64}
+      />
+      <Flex flexDirection="column" alignItems="flex-end">
+        <Heading mb="4px">{lpLabel.split(' ')[0]}</Heading>
+        <Flex justifyContent="center">
+          {isCommunityFarm ? <CommunityTag /> : <CoreTag />}
+          <MultiplierTag variant="secondary">{multiplier}</MultiplierTag>
+        </Flex>
+      </Flex>
+    </Wrapper>
+  )
+}
+
+export default CardHeading
